97d31e775b67e136f33f81586b6ea9f0e45400c8
lucaa <luca.abate03@gmail.com>
2025-04-25 19:09:14 +0200

    ready to merge in main

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,16 @@ import 'package:window_size/window_size.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const ToDoApp());
+  runApp(const Bloom());
 }
 
-class ToDoApp extends StatelessWidget {
-  const ToDoApp({Key? key}) : super(key: key);
+class Bloom extends StatelessWidget {
+  const Bloom({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AB',
+      title: 'Bloom',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.black,

7a990799a8ba80ea2ab268707c271e0a99faf99b
lucaa <luca.abate03@gmail.com>
2025-05-07 09:57:52 +0200

    mobile and desktop game appearance changed

--- lib/routes/Game/higher_or_lower.dart
+++ lib/routes/Game/higher_or_lower.dart
@@ -366,7 +366,7 @@ class _HigherOrLowerState extends State<HigherOrLower>
                         if (!_isGameOver && !_showQuestionBox) ...[
                           _buildGameButton(
                             bottom: screenHeight * 0.15,
-                            right: screenWidth * 0.3,
+                            right: screenWidth * 0.08,
                             text: "↑",
                             onPressed: () {
                               _checkGuess("up");
@@ -376,7 +376,7 @@ class _HigherOrLowerState extends State<HigherOrLower>
                           ),
                           _buildGameButton(
                             bottom: screenHeight * 0.15,
-                            right: screenWidth * 0.08,
+                            right: screenWidth * 0.3,
                             text: "↓",
                             onPressed: () {
                               _downController.forward(from: 0.0);
@@ -395,19 +395,19 @@ class _HigherOrLowerState extends State<HigherOrLower>
                         if (_showQuestionBox)
                           _buildQuestionBox(screenWidth * 2, screenHeight),
                         Positioned(
-                          top: screenHeight * 0.038,
+                          top: screenHeight * 0.095,
                           child: Text(
                             "Score: $score\nBest: $bestScore",
                             textAlign: TextAlign.center,
                             style: TextStyle(
-                              fontSize: screenHeight * 0.025,
+                              fontSize: screenHeight * 0.02,
                               fontFamily: 'RetroGaming',
                               color: Colors.white,
                             ),
                           ),
                         ),
                         Positioned(
-                          top: MediaQuery.of(context).size.height * 0.1,
+                          top: screenHeight * 0.15,
                           child: AnimatedOpacity(
                             opacity: _isDoublePoints ? 1.0 : 0.0,
                             duration: const Duration(milliseconds: 300),
@@ -417,6 +417,8 @@ class _HigherOrLowerState extends State<HigherOrLower>
                                 fontSize: 20,
                                 color: Colors.yellow,
                                 fontFamily: 'RetroGaming',
+                                //color in hex
+                                backgroundColor: Color(0xFF5460C1),
                                 shadows: [
                                   Shadow(
                                     blurRadius: 10,
@@ -486,7 +488,7 @@ class _HigherOrLowerState extends State<HigherOrLower>
                         if (_showQuestionBox)
                           _buildQuestionBox(screenWidth, screenHeight),
                         Positioned(
-                          top: screenHeight * 0.038,
+                          top: screenHeight * 0.133,
                           child: Text(
                             "Score: $score\nBest: $bestScore",
                             textAlign: TextAlign.center,
@@ -498,7 +500,7 @@ class _HigherOrLowerState extends State<HigherOrLower>
                           ),
                         ),
                         Positioned(
-                          top: MediaQuery.of(context).size.height * 0.1,
+                          top: screenHeight * 0.2,
                           child: AnimatedOpacity(
                             opacity: _isDoublePoints ? 1.0 : 0.0,
                             duration: const Duration(milliseconds: 300),
@@ -508,6 +510,8 @@ class _HigherOrLowerState extends State<HigherOrLower>
                                 fontSize: 20,
                                 color: Colors.yellow,
                                 fontFamily: 'RetroGaming',
+                                //color in hex
+                                backgroundColor: Color(0xFF5460C1),
                                 shadows: [
                                   Shadow(
                                     blurRadius: 10,
